eecc7fcc1423f56ad84d676f95ed8508bc428b2a
Juan Carlos Morales Brigidano <juancarlosmoralesbrigidano@gmail.com>
2015-06-08 21:46:46 +0200

    Basic functionality completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Spot.create address: 'Skatepark Manzanares, 28045 Madrid', latitude: 40.395473, longitude: -3.703388, level: 'Medium', floor_quality: 'Very good', teachers: 'Yes', photo: 'http://www.streetboardermag.com/wp-content/uploads/2013/08/arg.jpg'
+Spot.create address: 'Calle de Valdetorres de Jarama 1, 28043 Madrid', latitude: 40.472459, longitude: -3.6476884, level: 'Medium', floor_quality: 'Good', teachers: 'No', photo: 'http://www.go-skateboarding.com/wp-content/uploads/2010/06/imagen-1.jpg'
