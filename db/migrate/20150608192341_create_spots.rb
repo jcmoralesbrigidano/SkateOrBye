452d0f8f963c98c9d099ccf6e14a818e0ba7dc45
Juan Carlos Morales Brigidano <juancarlosmoralesbrigidano@gmail.com>
2015-06-08 21:27:35 +0200

    Create Spot migration

--- db/migrate/20150608192341_create_spots.rb
+++ db/migrate/20150608192341_create_spots.rb
@@ -1,7 +1,13 @@
 class CreateSpots < ActiveRecord::Migration
   def change
     create_table :spots do |t|
-
+      t.string :address
+      t.decimal :latitude
+      t.decimal :longitude
+      t.string :level
+      t.string :floor_quality
+      t.string :teachers
+      t.string :photo
       t.timestamps null: false
     end
   end
