2f4f442d54759a8139be2e524c0dfae236edc3d6
Juan Carlos Morales Brigidano <juancarlosmoralesbrigidano@gmail.com>
2015-06-08 21:34:56 +0200

    Spots views created

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,36 @@
 class SpotsController < ApplicationController
+	def index
+		@spots = Spot.all
+
+		respond_to do |format|
+			format.html 
+			format.json { render json: @spots }
+		end
+	end
+
+	def show
+		@spot = Spot.find params[:id]
+	end
+
+	def new
+		@spot = Spot.new
+
+		@latitude = params[:latitude]
+		@longitude = params[:longitude]
+	end
+
+	def create
+		@spot = Spot.new spot_params
+
+		if @spot.save
+			redirect_to spots_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def spot_params
+		params.require(:spot).permit(:latitude, :longitude, :level, :floor_quality, :teachers, :photo)
+	end
 end
