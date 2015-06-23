3f435c3f04a3f18682ba005691bb3e5e3e9f7cb3
Juan Carlos Morales Brigidano <johndoe@example.com>
2015-06-23 09:24:37 +0200

    Jams functionality fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ Spot.create address: 'Skatepark Manzanares, 28045 Madrid', latitude: 40.395473, 
 Spot.create address: 'Calle de Valdetorres de Jarama 1, 28043 Madrid', latitude: 40.472459, longitude: -3.6476884, level: 'Medium', floor_quality: 'Good', teachers: 'No', photo: 'http://www.go-skateboarding.com/wp-content/uploads/2010/06/imagen-1.jpg'
 Spot.create address: 'Calle Fco. de Goya, S/N 28660 Boadilla del Monte', latitude: 40.4037715, longitude: -3.8897478, level: 'Medium', floor_quality: 'Very good', teachers: 'Yes', photo: 'http://www.skateboard.com.au/images/matarord1.jpg'
 
-Skater.create name: 'admin', email: 'admin@skatetemple.com', password: 'admin'
+#Skater.create name: 'admin', email: 'admin@skatetemple.com', password: 'admin'
