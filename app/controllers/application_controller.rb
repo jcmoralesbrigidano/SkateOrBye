4b5396cb490967ff9f9559fa78251bc23652ce82
Juan Carlos Morales Brigidano <juancarlosmoralesbrigidano@gmail.com>
2015-07-16 04:45:16 +0200

    SkateOrBye! Beta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_skater, :pending_challenges
+  helper_method :current_skater, :pending_challenges, :completed_challenges
 
   def current_skater
 	  @current_skater ||= Skater.find session[:id] if session[:id]
@@ -17,6 +17,13 @@ class ApplicationController < ActionController::Base
     if session[:id]
       @skater = Skater.find session[:id]
       @pending_challenges = @skater.challenges.where(state: 'pending').count
+    end
+  end
+
+  def completed_challenges
+    if session[:id]
+      @skater = Skater.find session[:id]
+      @challenges_completed = Challenge.where(skater_id: @skater.id).where(state: 'completed').count
     end
   end
 end
